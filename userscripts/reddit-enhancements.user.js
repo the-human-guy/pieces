c6a7734f98385f89e2098d2f424b44792f63b621
the-human-guy <170432371+the-human-guy@users.noreply.github.com>
2025-03-05 23:34:42 +0300

    reddit userscript

--- userscripts/reddit-enhancements.user.js
+++ userscripts/reddit-enhancements.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        New script reddit.com
+// @name        Reddit oldify
 // @namespace   Violentmonkey Scripts
 // @match       https://*.reddit.com/*
 // @grant       none
@@ -8,10 +8,23 @@
 // @description 11/26/2024, 5:50:32 AM
 // ==/UserScript==
 
+const LINK_TO_OLD_ID = 'link-to-old'
+
+function modifyLink(oldLink) {
+  if (oldLink.includes('/gallery/')) {
+    const [x, postId] = oldLink.split('/gallery/')
+
+    return `/${postId}`
+  }
+
+  return oldLink.replace('www.', 'old.')
+}
+
 if (location.host.startsWith('www.')) {
   var btnContainer = document.createElement('div')
   btnContainer.style = "position: fixed; z-index: 99999; background: black; top: 0; left: 0"
   var linkToOld = document.createElement('a')
+  linkToOld.id = LINK_TO_OLD_ID
   linkToOld.href = location.href.replace('www.', 'old.')
   linkToOld.innerText = 'Switch to old.'
   btnContainer.appendChild(linkToOld)
@@ -22,7 +35,7 @@ if (location.host.startsWith('www.')) {
 function oldifyLinks() {
   const links = document.querySelectorAll('a[href*="www.reddit"]')
   console.log('oldifyLinks: ', links)
-  links.forEach(link => link.href = link.href.replace('www.', 'old.'))
+  links.forEach(link => link.href = modifyLink(link.href))
 }
 
 if (location.host.startsWith('old.')) {
