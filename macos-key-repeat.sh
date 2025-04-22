922940b3c0bfa97a89af2e5116f232bdfd687440
the-human-guy <170432371+the-human-guy@users.noreply.github.com>
2025-04-22 22:33:35 +0300

    macos-key-repeat.sh

---
+++ macos-key-repeat.sh
@@ -0,0 +1,4 @@
+defaults write -g ApplePressAndHoldEnabled -bool false
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+defaults write NSGlobalDomain KeyRepeat -float 1.5
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
